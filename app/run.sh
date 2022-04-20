dfc17f0735bec9a00eb7b7e957436da693448f5c
Dustin Lang <dstn@cloud2b.arbutus>
2022-04-20 15:54:23 +0000

    docker

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
 #--touch-reload wsgi.py --wsgi-file wsgi.py \
-    uwsgi --socket 0.0.0.0:3000 \
-      --touch-reload app.py \
-      --uid=www-data --gid=www-data \
-      --mount /=app:app
+
+#--socket 0.0.0.0:3000 \
+
+export PYTHONPATH=/app
+pwd
+ls -l
+find .
+#python3 -c "import app; print(app.__file__)"
+#     --touch-reload myapp.py \
+#      --mount /=myapp.app
+#       --plugin python3 \
+
+uwsgi \
+      --http 0.0.0.0:3000 \
+       --uid=www-data --gid=www-data \
+       --wsgi myapp \
+       --callable app
+
