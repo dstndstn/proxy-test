45bf7654a358ad43679cca5dedc1332ee3b719c1
Dustin Lang <dstndstn@gmail.com>
2022-04-20 12:19:13 -0700

    add uwsgi_params

--- app/myapp.py
+++ app/myapp.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<stuff>")
+def hello_world(stuff):
+    return '' + str(stuff)
 
 if __name__ == '__main__':
     app.run()
