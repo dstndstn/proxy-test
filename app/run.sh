8222c4879b354ba1c6475bbccb90827646762100
Dustin Lang <dstn@cloud2b.arbutus>
2022-04-20 18:36:31 +0000

    nersc

--- app/run.sh
+++ app/run.sh
@@ -1,21 +1,12 @@
 #! /bin/bash
 
-#--touch-reload wsgi.py --wsgi-file wsgi.py \
-
-#--socket 0.0.0.0:3000 \
+#      --http 0.0.0.0:3000 \
+#      --hook-master-start "unix_signal:1 gracefully_kill_them_all"
+#      --uid=www-data --gid=www-data \
 
 export PYTHONPATH=/app
-pwd
-ls -l
-find .
-#python3 -c "import app; print(app.__file__)"
-#     --touch-reload myapp.py \
-#      --mount /=myapp.app
-#       --plugin python3 \
-
 uwsgi \
-      --http 0.0.0.0:3000 \
-       --uid=www-data --gid=www-data \
-       --wsgi myapp \
-       --callable app
-
+      --socket 0.0.0.0:3000 \
+      --touch-reload myapp.py \
+      --module myapp \
+      --callable app
